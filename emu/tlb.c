cARLOSG42091/ish

Refactor for linux

emu/tlb.c
@@ -1,14 +1,15 @@
 #include "emu/cpu.h"
 #include "emu/tlb.h"
 
-void tlb_init(struct tlb *tlb, struct mem *mem) {
-    tlb->mem = mem;
+void tlb_init(struct tlb *tlb, struct mmu *mmu) {
+    tlb->mmu = mmu;
     tlb->dirty_page = TLB_PAGE_EMPTY;
-    tlb->mem_changes = mem->changes;
+    tlb->mem_changes = mmu->changes;
     tlb_flush(tlb);
 }
 
 void tlb_flush(struct tlb *tlb) {
+    tlb->mem_changes = tlb->mmu->changes;
     for (unsigned i = 0; i < TLB_SIZE; i++)
         tlb->entries[i] = (struct tlb_entry) {.page = 1, .page_if_writable = 1};
 }
@@ -42,12 +43,11 @@ bool __tlb_write_cross_page(struct tlb *tlb, addr_t addr, const char *value, uns
 }
 
 __no_instrument void *tlb_handle_miss(struct tlb *tlb, addr_t addr, int type) {
-    unsigned changes = tlb->mem->changes;
-    char *ptr = mem_ptr(tlb->mem, TLB_PAGE(addr), type);
+    char *ptr = mmu_translate(tlb->mmu, TLB_PAGE(addr), type);
+    if (tlb->mmu->changes != tlb->mem_changes)
+        tlb_flush(tlb);
     if (ptr == NULL)
         return NULL;
-    if (tlb->mem->changes != changes)
-        tlb_flush(tlb);
     tlb->dirty_page = TLB_PAGE(addr);
 
     struct tlb_entry *tlb_ent = &tlb->entries[TLB_INDEX(addr)];

emu/tlb.h
@@ -2,7 +2,7 @@
 #define TLB_H
 
 #include <string.h>
-#include "emu/memory.h"
+#include "emu/mmu.h"
 #include "debug.h"
 
 struct tlb_entry {
@@ -13,7 +13,7 @@ struct tlb_entry {
 #define TLB_BITS 10
 #define TLB_SIZE (1 << TLB_BITS)
 struct tlb {
-    struct mem *mem;
+    struct mmu *mmu;
     page_t dirty_page;
     unsigned mem_changes;
     struct tlb_entry entries[TLB_SIZE];
@@ -22,7 +22,7 @@ struct tlb {
 #define TLB_INDEX(addr) (((addr >> PAGE_BITS) & (TLB_SIZE - 1)) ^ (addr >> (PAGE_BITS + TLB_BITS)))
 #define TLB_PAGE(addr) (addr & 0xfffff000)
 #define TLB_PAGE_EMPTY 1
-void tlb_init(struct tlb *tlb, struct mem *mem);
+void tlb_init(struct tlb *tlb, struct mmu *mmu);
 void tlb_free(struct tlb *tlb);
 void tlb_flush(struct tlb *tlb);
 void *tlb_handle_miss(struct tlb *tlb, addr_t addr, int type);

kernel/task.c
@@ -95,9 +95,11 @@ void task_destroy(struct task *task) {
 void task_run_current() {
     struct cpu_state *cpu = &current->cpu;
     struct tlb tlb;
-    tlb_init(&tlb, current->mem);
+    tlb_init(&tlb, &current->mem->mmu);
     while (true) {
+        read_wrlock(&current->mem->lock);
         int interrupt = cpu_run_to_interrupt(cpu, &tlb);
+        read_wrunlock(&current->mem->lock);
         handle_interrupt(interrupt);
     }
 }

emkey1/aether

emu/tlb.c
@@ -1,16 +1,17 @@
 #include "emu/cpu.h"
 #include "emu/tlb.h"
 
-void tlb_refresh(struct tlb *tlb, struct mem *mem) {
-    if (tlb->mem == mem && tlb->mem_changes == mem->changes)
+void tlb_refresh(struct tlb *tlb, struct mmu *mmu) {
+    if (tlb->mmu == mmu && tlb->mem_changes == mmu->changes)
         return;
-    tlb->mem = mem;
+    tlb->mmu = mmu;
     tlb->dirty_page = TLB_PAGE_EMPTY;
-    tlb->mem_changes = mem->changes;
+    tlb->mem_changes = mmu->changes;
     tlb_flush(tlb);
 }
 
 void tlb_flush(struct tlb *tlb) {
+    tlb->mem_changes = tlb->mmu->changes;
     for (unsigned i = 0; i < TLB_SIZE; i++)
         tlb->entries[i] = (struct tlb_entry) {.page = 1, .page_if_writable = 1};
 }
@@ -44,12 +45,11 @@ bool __tlb_write_cross_page(struct tlb *tlb, addr_t addr, const char *value, uns
 }
 
 __no_instrument void *tlb_handle_miss(struct tlb *tlb, addr_t addr, int type) {
-    unsigned changes = tlb->mem->changes;
-    char *ptr = mem_ptr(tlb->mem, TLB_PAGE(addr), type);
+    char *ptr = mmu_translate(tlb->mmu, TLB_PAGE(addr), type);
+    if (tlb->mmu->changes != tlb->mem_changes)
+        tlb_flush(tlb);
     if (ptr == NULL)
         return NULL;
-    if (tlb->mem->changes != changes)
-        tlb_flush(tlb);
     tlb->dirty_page = TLB_PAGE(addr);
 
     struct tlb_entry *tlb_ent = &tlb->entries[TLB_INDEX(addr)];

emu/tlb.h
@@ -2,7 +2,7 @@
 #define TLB_H
 
 #include <string.h>
-#include "emu/memory.h"
+#include "emu/mmu.h"
 #include "debug.h"
 
 struct tlb_entry {
@@ -13,7 +13,7 @@ struct tlb_entry {
 #define TLB_BITS 10
 #define TLB_SIZE (1 << TLB_BITS)
 struct tlb {
-    struct mem *mem;
+    struct mmu *mmu;
     page_t dirty_page;
     unsigned mem_changes;
     struct tlb_entry entries[TLB_SIZE];
@@ -22,7 +22,7 @@ struct tlb {
 #define TLB_INDEX(addr) (((addr >> PAGE_BITS) & (TLB_SIZE - 1)) ^ (addr >> (PAGE_BITS + TLB_BITS)))
 #define TLB_PAGE(addr) (addr & 0xfffff000)
 #define TLB_PAGE_EMPTY 1
-void tlb_refresh(struct tlb *tlb, struct mem *mem);
+void tlb_refresh(struct tlb *tlb, struct mmu *mmu);
 void tlb_free(struct tlb *tlb);
 void tlb_flush(struct tlb *tlb);
 void *tlb_handle_miss(struct tlb *tlb, addr_t addr, int type);

kernel/task.c
@@ -98,9 +98,11 @@ void task_destroy(struct task *task) {
 void task_run_current() {
     struct cpu_state *cpu = &current->cpu;
     struct tlb tlb;
-    tlb_refresh(&tlb, current->mem);
+    tlb_refresh(&tlb, &current->mem->mmu);
     while (true) {
+        read_wrlock(&current->mem->lock);
         int interrupt = cpu_run_to_interrupt(cpu, &tlb);
+        read_wrunlock(&current->mem->lock);
         handle_interrupt(interrupt);
     }
 }