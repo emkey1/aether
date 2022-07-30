c4f6f4a0d3399a1aa96139ec9fde2e43e2dd4cbd
Jason Conway <jpc@jasonconway.dev>
2022-07-29 21:16:12 -0500

    Speedup edx -> flag translation

--- fs/proc/root.c
+++ fs/proc/root.c
@@ -18,91 +18,39 @@ static int proc_show_version(struct proc_entry *UNUSED(entry), struct proc_data 
 }
 
 char * parse_edx_flags(dword_t edx, char *edx_flags) { /* Translate edx bit flags into text */
-    if(edx & (1<<0))
-        strcat(edx_flags, "fpu ");
-    if(edx & (1<<1))
-        strcat(edx_flags, "vme ");
-    if(edx & (1<<2))
-        strcat(edx_flags, "de ");
-    if(edx & (1<<3))
-        strcat(edx_flags, "pse ");
-    if(edx & (1<<4))
-        strcat(edx_flags, "tsc ");
-    if(edx & (1<<5))
-        strcat(edx_flags, "msr ");
-    if(edx & (1<<6))
-        strcat(edx_flags, "pae ");
-    if(edx & (1<<7))
-        strcat(edx_flags, "mce ");
-    if(edx & (1<<8))
-        strcat(edx_flags, "cx8 ");
-    if(edx & (1<<9))
-        strcat(edx_flags, "apic ");
-    if(edx & (1<<10))
-        strcat(edx_flags, "Reserved ");
-    if(edx & (1<<11))
-        strcat(edx_flags, "sep ");
-    if(edx & (1<<12))
-        strcat(edx_flags, "mtrr ");
-    if(edx & (1<<13))
-        strcat(edx_flags, "pge ");
-    if(edx & (1<<14))
-        strcat(edx_flags, "mca ");
-    if(edx & (1<<15))
-        strcat(edx_flags, "cmov ");
-    if(edx & (1<<17))
-        strcat(edx_flags, "pse-36 ");
-    if(edx & (1<<18))
-        strcat(edx_flags, "psn ");
-    if(edx & (1<<19))
-        strcat(edx_flags, "clfsh ");
-    if(edx & (1<<20))
-        strcat(edx_flags, "Reserved ");
-    if(edx & (1<<21))
-        strcat(edx_flags, "ds ");
-    if(edx & (1<<22))
-        strcat(edx_flags, "acpi ");
-    if(edx & (1<<23))
-        strcat(edx_flags, "mmx ");
-    if(edx & (1<<24))
-        strcat(edx_flags, "fxsr ");
-    if(edx & (1<<25))
-        strcat(edx_flags, "sse ");
-    if(edx & (1<<26))
-        strcat(edx_flags, "sse2 ");
-    if(edx & (1<<27))
-        strcat(edx_flags, "ss ");
-    if(edx & (1<<28))
-        strcat(edx_flags, "htt ");
-    if(edx & (1<<29))
-        strcat(edx_flags, "tm ");
-    if(edx & (1<<30))
-        strcat(edx_flags, "Reserved ");
-    if(edx & (1<<31))
-        strcat(edx_flags, "pbe ");
+    static const char *enumerated[] = {
+        "fpu ", "vme ", "de ", "pse ", "tsc ", "msr ", "pae ", "mce ", "cx8 ", "apic ", "Reserved ",
+        "sep ", "mtrr ", "pge ", "mca ", "cmov ", "", "pse-36 ", "psn ", "clfsh ", "Reserved ",
+        "ds ", "acpi ", "mmx ", "fxsr ", "sse ", "sse2 ", "ss ", "htt ", "tm ", "Reserved ", "pbe "
+    };
 
-    strcat(edx_flags, "\0");
-    return(edx_flags);
+    size_t offset = 0;
+    for (size_t i = 0; i < 32; i++) {
+        if (edx & (1 << i)) {
+            const size_t enum_len = strlen(enumerated[i]);
+            memcpy(edx_flags + offset, enumerated[i], enum_len);
+            offset += enum_len;
+        }
+    }
+    edx_flags[offset] = '\0';
+    return edx_flags;
 }
 
-char * translate_vendor_id(dword_t *ebx, dword_t *ecx, dword_t *edx) {
+static void unpack32(dword_t src, void *dst) {
+    unsigned char *p = dst;
+    for (size_t i = 0; i < 4; i++) {
+        p[i] = (src >> (0x08 * i)) & 0xff;
+    }
+}
+
+char *translate_vendor_id(dword_t *ebx, dword_t *ecx, dword_t *edx) {
     char *byteArray = calloc(12 + 1, sizeof(char)); // vendor_id is fixed at 12 bytes
 
-    // convert from an unsigned long int to a 4-byte array
-    byteArray[0] = (int)((*ebx & 0XFF));
-    byteArray[1] = (int)((*ebx >> 8) & 0XFF);
-    byteArray[2] = (int)((*ebx >> 16) & 0xFF) ;
-    byteArray[3] = (int)((*ebx >> 24) & 0xFF) ;
-    byteArray[4] = (int)((*edx & 0XFF));
-    byteArray[5] = (int)((*edx >> 8) & 0XFF);
-    byteArray[6] = (int)((*edx >> 16) & 0xFF) ;
-    byteArray[7] = (int)((*edx >> 24) & 0xFF) ;
-    byteArray[8] = (int)((*ecx & 0XFF));
-    byteArray[9] = (int)((*ecx >> 8) & 0XFF);
-    byteArray[10] = (int)((*ecx >> 16) & 0xFF) ;
-    byteArray[11] = (int)((*ecx >> 24) & 0xFF) ;
-    byteArray[12] = 0;
-    return(byteArray);
+    unpack32(*ebx, &byteArray[0]);
+    unpack32(*edx, &byteArray[4]);
+    unpack32(*ecx, &byteArray[8]);
+
+    return byteArray;
 }
 
 static int proc_show_cpuinfo(struct proc_entry *UNUSED(entry), struct proc_data *buf) {
