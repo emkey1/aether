e72055f5d963ebb438788c5f472d32529032183d
Mike Miller <emkey1@oneopa.com>
2022-10-27 15:49:34 -0700

    Some cleanup/fixing in regards to lock->comm, plus cleanup and bug fixing of a bogus unlock of &pid_lock that I introduced in August.

--- util/sync.c
+++ util/sync.c
@@ -31,145 +31,6 @@ static bool is_signal_pending(lock_t *lock) {
     return pending;
 }
 
-int wait_for(cond_t *cond, lock_t *lock, struct timespec *timeout) {
-    if (is_signal_pending(lock))
-        return _EINTR;
-    int err = wait_for_ignore_signals(cond, lock, timeout);
-    if (err < 0)
-        return _ETIMEDOUT;
-    if (is_signal_pending(lock))
-        return _EINTR;
-    return 0;
-}
-
-int wait_for_ignore_signals(cond_t *cond, lock_t *lock, struct timespec *timeout) {
-    if (current) {
-        lock(&current->waiting_cond_lock, 0);
-        current->waiting_cond = cond;
-        current->waiting_lock = lock;
-        unlock(&current->waiting_cond_lock);
-    }
-        
-    int rc = 0;
-#if LOCK_DEBUG
-    struct lock_debug lock_tmp = lock->debug;
-    lock->debug = (struct lock_debug) { .initialized = lock->debug.initialized };
-#endif
-    unsigned attempts = 0;
-    if (!timeout) {
-        if(current->pid <= 20) {
-            pthread_cond_wait(&cond->cond, &lock->m);
-            goto SKIP;
-        }
-    AGAIN:
-        if(lock->pid == -1) {  // Something has gone wrong.  -mke
-            lock(&current->waiting_cond_lock, 0);
-            current->waiting_cond = NULL;
-            current->waiting_lock = NULL;
-            unlock(&current->waiting_cond_lock);
-            //return _ETIMEDOUT;
-            printk("ERROR: Locking PID is gone in wait_for_ignore_signals() (%s:%d).  Attempting recovery", current->comm, current->pid);
-            return 0;
-            // Weird
-        }
-        struct timespec trigger_time;
-        clock_gettime(CLOCK_MONOTONIC, &trigger_time);
-        trigger_time.tv_sec = trigger_time.tv_sec + 6;
-        trigger_time.tv_nsec = 0;
-        rc = pthread_cond_timedwait_relative_np(&cond->cond, &lock->m, &trigger_time);
-        if(rc == ETIMEDOUT) {
-            attempts++;
-            if(attempts <= 6)  // We are likely deadlocked if more than ten attempts -mke
-                goto AGAIN;
-            printk("ERROR: Deadlock in wait_for_ignore_signals() (%s:%d).  Attempting recovery", current->comm, current->pid);
-            //return _ETIMEDOUT;
-            return 0;
-        }
-    } else {
-#if __linux__
-        struct timespec abs_timeout;
-        clock_gettime(CLOCK_MONOTONIC, &abs_timeout);
-        abs_timeout.tv_sec += timeout->tv_sec;
-        abs_timeout.tv_nsec += timeout->tv_nsec;
-        if (abs_timeout.tv_nsec > 1000000000) {
-            abs_timeout.tv_sec++;
-            abs_timeout.tv_nsec -= 1000000000;
-        }
-        rc = pthread_cond_timedwait(&cond->cond, &lock->m, &abs_timeout);
-#elif __APPLE__
-        rc = pthread_cond_timedwait_relative_np(&cond->cond, &lock->m, timeout);
-#else
-#error Unimplemented pthread_cond_wait relative timeout.
-#endif
-    }
-    
-SKIP:
-#if LOCK_DEBUG
-    lock->debug = lock_tmp;
-#endif
-
-    if (current) {
-        lock(&current->waiting_cond_lock, 0);
-        current->waiting_cond = NULL;
-        current->waiting_lock = NULL;
-        unlock(&current->waiting_cond_lock);
-    }
-    if (rc == ETIMEDOUT)
-        return _ETIMEDOUT;
-    return 0;
-}
-
-void notify(cond_t *cond) {
-    pthread_cond_broadcast(&cond->cond);
-}
-void notify_once(cond_t *cond) {
-    pthread_cond_signal(&cond->cond);
-}
-
-__thread sigjmp_buf unwind_buf;
-__thread bool should_unwind = false;
-
-void sigusr1_handler(void) {
-    if (should_unwind) {
-        should_unwind = false;
-        siglongjmp(unwind_buf, 1);
-    }
-}
-
-// Because sometimes we can't #include "kernel/task.h" -mke
-unsigned critical_region_count(struct task *task) {
-    unsigned tmp = 0;
-//    pthread_mutex_lock(task->critical_region.lock); // This would make more
-    tmp = task->critical_region.count;
- //   pthread_mutex_unlock(task->critical_region.lock);
-
-    return tmp;
-}
-
-unsigned critical_region_count_wrapper() { // sync.h can't know about the definition of struct due to recursive include files.  -mke
-    return(critical_region_count(current));
-}
-unsigned locks_held_count(struct task *task) {
-   // return 0; // Short circuit for now
-    if(task->pid < 10)  // Here be monsters.  -mke
-        return 0;
-    if(task->locks_held.count > 0) {
-        return(task->locks_held.count -1);
-    }
-    unsigned tmp = 0;
-    pthread_mutex_lock(&task->locks_held.lock);
-    tmp = task->locks_held.count;
-    pthread_mutex_unlock(&task->locks_held.lock);
-
-    return tmp;
-}
-
-unsigned locks_held_count_wrapper() { // sync.h can't know about the definition of struct due to recursive include files.  -mke
-    if(current != NULL)
-        return(locks_held_count(current));
-    return 0;
-}
-
 void modify_critical_region_counter(struct task *task, int value, __attribute__((unused)) const char *file, __attribute__((unused)) int line) { // value Should only be -1 or 1.  -mke
     if(task == NULL) {
         if(current != NULL) {
@@ -230,6 +91,162 @@ void modify_locks_held_count_wrapper(int value) { // sync.h can't know about the
         modify_locks_held_count(current, value);
     return;
 }
+
+int wait_for(cond_t *cond, lock_t *lock, struct timespec *timeout) {
+    if (is_signal_pending(lock))
+        return _EINTR;
+    int err = wait_for_ignore_signals(cond, lock, timeout);
+    if (err < 0)
+        return _ETIMEDOUT;
+    if (is_signal_pending(lock))
+        return _EINTR;
+    return 0;
+}
+
+int wait_for_ignore_signals(cond_t *cond, lock_t *lock, struct timespec *timeout) {
+    
+    if (current) {
+        lock(&current->waiting_cond_lock, 0);
+        current->waiting_cond = cond;
+        current->waiting_lock = lock;
+        unlock(&current->waiting_cond_lock);
+    }
+    
+    modify_critical_region_counter(current, 1,__FILE__, __LINE__);
+    int savepid = current->pid;
+        
+    int rc = 0;
+#if LOCK_DEBUG
+    struct lock_debug lock_tmp = lock->debug;
+    lock->debug = (struct lock_debug) { .initialized = lock->debug.initialized };
+#endif
+    unsigned attempts = 0;
+    if (!timeout) {
+        if(current->pid <= 20) {
+            pthread_cond_wait(&cond->cond, &lock->m);
+            goto SKIP;
+        }
+    AGAIN:
+        if((lock->pid == -1) && (lock->comm[0] == 0)) {  // Something has gone wrong.  -mke
+            lock(&current->waiting_cond_lock, 0);
+            current->waiting_cond = NULL;
+            current->waiting_lock = NULL;
+            unlock(&current->waiting_cond_lock);
+            //return _ETIMEDOUT;
+            printk("ERROR: Locking PID is gone in wait_for_ignore_signals() (%s:%d).  Attempting recovery\n", current->comm, current->pid);
+            unlock(lock);
+            modify_critical_region_counter(current, -1,__FILE__, __LINE__);
+            return 0;
+            // Weird
+        }
+        struct timespec trigger_time;
+        clock_gettime(CLOCK_MONOTONIC, &trigger_time);
+        trigger_time.tv_sec = trigger_time.tv_sec + 6;
+        trigger_time.tv_nsec = 0;
+        if((lock->pid == -1) && (lock->comm[0] == 0)) {  // Something has gone wrong.  -mke
+            goto AGAIN;  // Ugh.  -mke
+        }
+        rc = pthread_cond_timedwait_relative_np(&cond->cond, &lock->m, &trigger_time);
+        if(rc == ETIMEDOUT) {
+            attempts++;
+            if(attempts <= 6)  // We are likely deadlocked if more than ten attempts -mke
+                goto AGAIN;
+            printk("ERROR: Deadlock in wait_for_ignore_signals() (%s:%d).  Attempting recovery\n", current->comm, current->pid);
+            unlock(lock);
+            modify_critical_region_counter(current, -1,__FILE__, __LINE__);
+            return 0;
+        } else if(rc == 0) {
+            goto SKIP;
+        }
+    } else {
+#if __linux__
+        struct timespec abs_timeout;
+        clock_gettime(CLOCK_MONOTONIC, &abs_timeout);
+        abs_timeout.tv_sec += timeout->tv_sec;
+        abs_timeout.tv_nsec += timeout->tv_nsec;
+        if (abs_timeout.tv_nsec > 1000000000) {
+            abs_timeout.tv_sec++;
+            abs_timeout.tv_nsec -= 1000000000;
+        }
+        rc = pthread_cond_timedwait(&cond->cond, &lock->m, &abs_timeout);
+#elif __APPLE__
+        rc = pthread_cond_timedwait_relative_np(&cond->cond, &lock->m, timeout);
+#else
+#error Unimplemented pthread_cond_wait relative timeout.
+#endif
+    }
+    
+SKIP:
+#if LOCK_DEBUG
+    lock->debug = lock_tmp;
+#endif
+
+    if (current) {
+        lock(&current->waiting_cond_lock, 0);
+        current->waiting_cond = NULL;
+        current->waiting_lock = NULL;
+        unlock(&current->waiting_cond_lock);
+    }
+    if (rc == ETIMEDOUT) {
+        modify_critical_region_counter(current, -1,__FILE__, __LINE__);
+        return _ETIMEDOUT;
+    }
+    
+    modify_critical_region_counter(current, -1,__FILE__, __LINE__);
+    return 0;
+}
+
+void notify(cond_t *cond) {
+    pthread_cond_broadcast(&cond->cond);
+}
+void notify_once(cond_t *cond) {
+    pthread_cond_signal(&cond->cond);
+}
+
+__thread sigjmp_buf unwind_buf;
+__thread bool should_unwind = false;
+
+void sigusr1_handler(void) {
+    if (should_unwind) {
+        should_unwind = false;
+        siglongjmp(unwind_buf, 1);
+    }
+}
+
+// Because sometimes we can't #include "kernel/task.h" -mke
+unsigned critical_region_count(struct task *task) {
+    unsigned tmp = 0;
+//    pthread_mutex_lock(task->critical_region.lock); // This would make more
+    tmp = task->critical_region.count;
+ //   pthread_mutex_unlock(task->critical_region.lock);
+
+    return tmp;
+}
+
+unsigned critical_region_count_wrapper() { // sync.h can't know about the definition of struct due to recursive include files.  -mke
+    return(critical_region_count(current));
+}
+unsigned locks_held_count(struct task *task) {
+   // return 0; // Short circuit for now
+    if(task->pid < 10)  // Here be monsters.  -mke
+        return 0;
+    if(task->locks_held.count > 0) {
+        return(task->locks_held.count -1);
+    }
+    unsigned tmp = 0;
+    pthread_mutex_lock(&task->locks_held.lock);
+    tmp = task->locks_held.count;
+    pthread_mutex_unlock(&task->locks_held.lock);
+
+    return tmp;
+}
+
+unsigned locks_held_count_wrapper() { // sync.h can't know about the definition of struct due to recursive include files.  -mke
+    if(current != NULL)
+        return(locks_held_count(current));
+    return 0;
+}
+
 
 // This is how you would mitigate the unlock/wait race if the wait
 // is async signal safe. wait_for *should* be safe from this race
