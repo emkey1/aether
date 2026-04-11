f44ef5b01f5e7a1096cdd06a0fa960ac0f779655
Mike Miller <emkey1@oneopa.com>
2026-04-11 11:28:53 -0700

    Add amd64 movq xmm load form

--- emu/amd64_interp.c
+++ emu/amd64_interp.c
@@ -976,7 +976,7 @@ restart_prefix:
             amd64_reg_set(cpu, modrm.reg, dst_size, src);
             break;
         }
-        if (op2 == 0x10 || op2 == 0x11 || op2 == 0x28 || op2 == 0x29 || op2 == 0x6c || op2 == 0x6e || op2 == 0x6f || op2 == 0x7f || op2 == 0xef) {
+        if (op2 == 0x10 || op2 == 0x11 || op2 == 0x28 || op2 == 0x29 || op2 == 0x6c || op2 == 0x6e || op2 == 0x6f || op2 == 0x7e || op2 == 0x7f || op2 == 0xef) {
             struct amd64_modrm modrm;
             union xmm_reg value;
             union xmm_reg src_xmm;
@@ -1010,6 +1010,18 @@ restart_prefix:
                     value.qw[0] = src_scalar;
                 else
                     value.u32[0] = (uint32_t) src_scalar;
+                cpu->xmm[modrm.reg] = value;
+            } else if (op2 == 0x7e) {
+                if (rep_mode != AMD64_REPZ)
+                    return INT_UNDEFINED;
+                value.u128 = 0;
+                if (modrm.is_reg) {
+                    value.qw[0] = cpu->xmm[modrm.rm].qw[0];
+                } else {
+                    if (!amd64_read_rm(cpu, tlb, &modrm, fs_prefix, 64, &src_scalar))
+                        goto amd64_gpf_restore;
+                    value.qw[0] = src_scalar;
+                }
                 cpu->xmm[modrm.reg] = value;
             } else if (op2 == 0xef) {
                 if (!operand_size_prefix)
