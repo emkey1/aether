a51a19f3249441d290d8a473e7f9bbd651e6f7e5
Mike Miller <emkey1@oneopa.com>
2026-04-11 13:25:35 -0700

    Unify amd64 movd movq opcode dispatch

--- emu/amd64_interp.c
+++ emu/amd64_interp.c
@@ -1008,7 +1008,36 @@ restart_prefix:
             amd64_reg_set(cpu, modrm.reg, dst_size, src);
             break;
         }
-        if (op2 == 0x10 || op2 == 0x11 || op2 == 0x16 || op2 == 0x17 || op2 == 0x28 || op2 == 0x29 || op2 == 0x62 || op2 == 0x6c || (op2 == 0x6e && operand_size_prefix) || op2 == 0x6f || op2 == 0x70 || op2 == 0x7e || op2 == 0x7f || op2 == 0xc6 || op2 == 0xd6 || op2 == 0xeb || op2 == 0xef) {
+        if (op2 == 0x6e) {
+            struct amd64_modrm modrm;
+            qword_t src_scalar;
+            if (!amd64_decode_modrm(cpu, tlb, rex, &modrm)) {
+                cpu->amd64_rip = saved_rip;
+                cpu->segfault_addr = (addr_t) saved_rip;
+                return INT_GPF;
+            }
+            if (operand_size_prefix) {
+                union xmm_reg value;
+                if (modrm.reg >= 8 || (modrm.is_reg && modrm.rm >= 8))
+                    return INT_UNDEFINED;
+                if (!amd64_read_rm(cpu, tlb, &modrm, fs_prefix, rex.w ? 64 : 32, &src_scalar))
+                    goto amd64_gpf_restore;
+                value.u128 = 0;
+                if (rex.w)
+                    value.qw[0] = src_scalar;
+                else
+                    value.u32[0] = (uint32_t) src_scalar;
+                cpu->xmm[modrm.reg] = value;
+            } else {
+                if (modrm.reg >= 8)
+                    return INT_UNDEFINED;
+                if (!amd64_read_rm(cpu, tlb, &modrm, fs_prefix, rex.w ? 64 : 32, &src_scalar))
+                    goto amd64_gpf_restore;
+                cpu->mm[modrm.reg].qw = rex.w ? src_scalar : (uint32_t) src_scalar;
+            }
+            break;
+        }
+        if (op2 == 0x10 || op2 == 0x11 || op2 == 0x16 || op2 == 0x17 || op2 == 0x28 || op2 == 0x29 || op2 == 0x62 || op2 == 0x6c || op2 == 0x6f || op2 == 0x70 || op2 == 0x7e || op2 == 0x7f || op2 == 0xc6 || op2 == 0xd6 || op2 == 0xeb || op2 == 0xef) {
             struct amd64_modrm modrm;
             union xmm_reg value;
             union xmm_reg src_xmm;
@@ -1060,17 +1089,6 @@ restart_prefix:
                 value.u32[1] = src_xmm.u32[0];
                 value.u32[2] = dst.u32[1];
                 value.u32[3] = src_xmm.u32[1];
-                cpu->xmm[modrm.reg] = value;
-            } else if (op2 == 0x6e) {
-                if (!operand_size_prefix)
-                    return INT_UNDEFINED;
-                if (!amd64_read_rm(cpu, tlb, &modrm, fs_prefix, rex.w ? 64 : 32, &src_scalar))
-                    goto amd64_gpf_restore;
-                value.u128 = 0;
-                if (rex.w)
-                    value.qw[0] = src_scalar;
-                else
-                    value.u32[0] = (uint32_t) src_scalar;
                 cpu->xmm[modrm.reg] = value;
             } else if (op2 == 0x7e) {
                 if (rep_mode != AMD64_REPZ)
@@ -1267,23 +1285,6 @@ restart_prefix:
             amd64_reg_set(cpu, modrm.reg, op_size, result);
             overflow = signed_result != amd64_sign_extend(result, op_size);
             amd64_set_mul_flags(cpu, overflow);
-            break;
-        }
-        if (op2 == 0x6e) {
-            struct amd64_modrm modrm;
-            qword_t src;
-            if (operand_size_prefix)
-                return INT_UNDEFINED;
-            if (!amd64_decode_modrm(cpu, tlb, rex, &modrm)) {
-                cpu->amd64_rip = saved_rip;
-                cpu->segfault_addr = (addr_t) saved_rip;
-                return INT_GPF;
-            }
-            if (modrm.reg >= 8)
-                return INT_UNDEFINED;
-            if (!amd64_read_rm(cpu, tlb, &modrm, fs_prefix, rex.w ? 64 : 32, &src))
-                goto amd64_gpf_restore;
-            cpu->mm[modrm.reg].qw = rex.w ? src : (uint32_t) src;
             break;
         }
         if (op2 == 0xb1) {
