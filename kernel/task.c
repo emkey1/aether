ab9d4546948335b239ed0d980d6e18418811e322
Mike Miller <emkey1@oneopa.com>
2026-04-11 09:53:35 -0700

    Log build timestamp in boot banner

--- kernel/task.c
+++ kernel/task.c
@@ -299,7 +299,8 @@ void run_at_boot(void) {  // Stuff we run only once, at boot time.
     lock_init(&pids_lock, "pids");
     lock_init(&block_lock, "block");
     lock_init(&atomic_l_lock, "run_at_boot");
-    printk("iSH-AOK %s booted on %d emulated x86 CPU(s)\n", uts.release, ncpu);
+    printk("iSH-AOK %s built %s %s booted on %d emulated x86 CPU(s)\n",
+            uts.release, __DATE__, __TIME__, ncpu);
     // Get boot time
     extern time_t boot_time;
          
